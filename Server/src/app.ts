ab033c4c6721711c6146e0721e5b4cde71a89c72
Dev-Tuhin01 <tuhinshubhrachakraborty2002@gmail.com>
2025-05-30 21:45:41 +0530

    Feat: Added misc route

--- Server/src/app.ts
+++ Server/src/app.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import authRouter from "./routes/auth.route.ts";
 import petRoutes from "./routes/pet.routes.ts";
 import choreRoute from "./routes/chore.route.ts";
+import miscRouter from "./routes/misc.route.ts";
 
 
 const app = express();
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use("/api/auth",authRouter);
 app.use("/api/pet",petRoutes);
 app.use("/api/chore",choreRoute);
+app.use("/api/misc/",miscRouter);
 
 
 export default app;
